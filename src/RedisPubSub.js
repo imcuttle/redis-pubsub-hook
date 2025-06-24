4b31f507098d8e12c7f1ed50e91030ed0d94716f
Cong Yu <moyuyc95@gmail.com>
2025-06-24 14:44:46 +0800

    Update RedisPubSub.js

--- src/RedisPubSub.js
+++ src/RedisPubSub.js
@@ -17,85 +17,85 @@
  * 根据以上的逻辑，所以我们传递的数据需要是可以被序列化的/反序列化回来的
  *
  */
-const RedisClustr = require('redis-clustr')
-const { createClient, RedisClient } = require('redis')
-const uuid = require('uuid').v4
-const { promisify } = require('util')
-const { parse } = require('url')
+const RedisClustr = require('redis-clustr');
+const { createClient, RedisClient } = require('redis');
+const uuid = require('uuid').v4;
+const { promisify } = require('util');
+const { parse } = require('url');
 
 // 获取信道的总共被订阅数量
 function defaultGetNumSub(channel, pub) {
   // RedisClustr
   if (pub instanceof RedisClustr) {
-    const ps = Object.keys(pub.connections).map((name) => {
-      const con = pub.connections[name]
-      return getNumSub(channel, con)
-    })
-    return Promise.all(ps).then((numList) => {
+    const ps = Object.keys(pub.connections).map(name => {
+      const con = pub.connections[name];
+      return getNumSub(channel, con);
+    });
+    return Promise.all(ps).then(numList => {
       // console.log('numList', numList)
-      return numList.reduce((acc, n) => acc + n)
-    })
+      return numList.reduce((acc, n) => acc + n);
+    });
   }
 
   if (pub.constructor.name !== 'Cluster') {
     // RedisClient or Redis
     return new Promise(function (resolve, reject) {
       pub.send_command('pubsub', ['numsub', channel], function (err, numsub) {
-        if (err) return reject(err)
-        resolve(parseInt(numsub[1], 10))
-      })
-    })
+        if (err) return reject(err);
+        resolve(parseInt(numsub[1], 10));
+      });
+    });
   } else {
     // Cluster
-    const nodes = pub.nodes()
+    const nodes = pub.nodes();
     return Promise.all(
       nodes.map(function (node) {
-        return node.send_command('pubsub', ['numsub', channel])
+        return node.send_command('pubsub', ['numsub', channel]);
       })
     ).then(function (values) {
-      let numsub = 0
+      let numsub = 0;
       values.map(function (value) {
-        numsub += parseInt(value[1], 10)
-      })
-      return numsub
-    })
+        numsub += parseInt(value[1], 10);
+      });
+      return numsub;
+    });
   }
 }
 
-const defaultCreateClient = (redisServers) => {
+const defaultCreateClient = redisServers => {
   if (redisServers instanceof RedisClustr || redisServers instanceof RedisClient) {
-    return redisServers
+    return redisServers;
   }
 
   if (typeof redisServers === 'string') {
-    redisServers = redisServers.split(',')
+    redisServers = redisServers.split(',');
   }
 
   if (Array.isArray(redisServers)) {
-    redisServers = redisServers.map((url) => {
+    redisServers = redisServers.map(url => {
       if (typeof url !== 'string') {
-        return url
+        return url;
       }
-      const obj = parse(url)
+      const obj = parse(url);
       return {
         port: obj.port,
-        host: obj.hostname
-      }
-    })
+        host: obj.hostname,
+      };
+    });
   } else {
-    return createClient(redisServers)
+    return createClient(redisServers);
   }
 
   if (redisServers.length > 1) {
-    return new RedisClustr(redisServers)
+    return new RedisClustr(redisServers);
   } else {
-    return createClient(redisServers[0])
+    return createClient(redisServers[0]);
   }
-}
+};
 
 class RedisPubSub {
-  static defaultCreateClient = defaultCreateClient
-  static defaultGetNumSub = defaultGetNumSub
+  static defaultCreateClient = defaultCreateClient;
+  static defaultGetNumSub = defaultGetNumSub;
 
   constructor({
     pub,
@@ -108,101 +108,101 @@ class RedisPubSub {
     // 超时 ms 时间
     timeout = 3000,
     requestChannel = 'RequestChannel',
-    responseChannel = 'ResponseChannel'
+    responseChannel = 'ResponseChannel',
   } = {}) {
-    this.requestChannel = requestChannel
-    this.responseChannel = responseChannel
-    this.hooks = hooks || {}
-    this.timeout = timeout
-    this.getNumSub = getNumSub
-    this.uid = uid
-    this.pub = pub || createClient(redis)
-    this.sub = sub || createClient(redis)
+    this.requestChannel = requestChannel;
+    this.responseChannel = responseChannel;
+    this.hooks = hooks || {};
+    this.timeout = timeout;
+    this.getNumSub = getNumSub;
+    this.uid = uid;
+    this.pub = pub || createClient(redis);
+    this.sub = sub || createClient(redis);
 
-    this.sub.subscribe([this.responseChannel, this.requestChannel])
-    this.onRequest = this.onRequest.bind(this)
-    this.sub.on('message', this.onRequest)
+    this.sub.subscribe([this.responseChannel, this.requestChannel]);
+    this.onRequest = this.onRequest.bind(this);
+    this.sub.on('message', this.onRequest);
   }
 
   async _runHook(source, type, args = []) {
     if (this.hooks.hasOwnProperty(type)) {
-      const hook = this.hooks[type]
-      return await hook.apply(null, [source].concat(args))
+      const hook = this.hooks[type];
+      return await hook.apply(null, [source].concat(args));
     } else {
-      console.error('Hook: ', type, 'not found!')
+      console.error('Hook: ', type, 'not found!');
     }
   }
 
   async onRequest(channel, data) {
     if (channel === this.requestChannel) {
-      const { type, args, uid } = JSON.parse(data)
-      if (this.uid === uid) {
-        return
+      const { type, args, uid, from } = JSON.parse(data);
+      if (this.uid === from) {
+        return;
       }
 
-      const output = await this._runHook('remote', type, args)
-      this.pub.publish(this.responseChannel, JSON.stringify({ res: output, uid }))
+      const output = await this._runHook('remote', type, args);
+      this.pub.publish(this.responseChannel, JSON.stringify({ type, res: output, uid, from: this.uid }));
     }
   }
 
   async request(type, ...args) {
-    let resolve
-    let timer
+    let resolve;
+    let timer;
     // 本地节点则不需要通过 redis pubsub，直接本地执行即可
-    const resList = [await this._runHook('local', type, args)]
+    const resList = [await this._runHook('local', type, args)];
 
-    const numSub = await this.getNumSub(this.responseChannel, this.pub)
+    const numSub = await this.getNumSub(this.responseChannel, this.pub);
     // console.log('numSub', numSub, this.responseChannel)
     if (resList.length === numSub) {
-      return resList
+      return resList;
     }
 
+    const uid = uuid();
     const onRequest = async (channel, data) => {
       if (channel === this.responseChannel) {
         // console.log('received responseChannel', data);
-        const { res, uid } = JSON.parse(data)
-        if (this.uid !== uid) {
-          return
+        const { res, uid: _uid, type: _type } = JSON.parse(data);
+        if (_uid !== uid) {
+          return;
         }
 
-        resList.push(res)
+        resList.push(res);
         if (resList.length === numSub) {
-          resolve(resList)
+          resolve(resList);
         }
       }
-    }
+    };
 
-    this.sub.on('message', onRequest)
-    this.pub.publish(this.requestChannel, JSON.stringify({ type, args, uid: this.uid }))
-
-    return new Promise((solve) => {
-      resolve = solve
+    return new Promise(solve => {
+      resolve = solve;
+      this.sub.on('message', onRequest);
+      this.pub.publish(this.requestChannel, JSON.stringify({ type, args, from: this.uid, uid }));
       timer = setTimeout(() => {
-        resolve(resList)
-      }, this.timeout)
+        resolve(resList);
+      }, this.timeout);
     }).finally(() => {
-      clearTimeout(timer)
-      this.sub.off('message', onRequest)
-    })
+      clearTimeout(timer);
+      this.sub.off('message', onRequest);
+    });
   }
 
   setHook(name, fn) {
-    this.hooks[name] = fn
+    this.hooks[name] = fn;
   }
 
   clear() {
-    this.sub.off('message', this.onRequest)
+    this.sub.off('message', this.onRequest);
 
-    return promisify(this.sub.unsubscribe.bind(this.sub))([this.requestChannel, this.responseChannel])
+    return promisify(this.sub.unsubscribe.bind(this.sub))([this.requestChannel, this.responseChannel]);
   }
 
   async quit() {
     try {
-      await this.clear()
+      await this.clear();
     } catch (e) {}
-    this.sub.quit()
-    this.pub.quit()
+    this.sub.quit();
+    this.pub.quit();
   }
 }
 
-module.exports = RedisPubSub
+module.exports = RedisPubSub;
